b12b233490e918048dad97c811b07837c6a897b3
yangzhujun <yangzhujun1@gmail.com>
2016-12-21 14:29:01 +0800

    add user_id to group

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221060948) do
+ActiveRecord::Schema.define(version: 20161221062504) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
